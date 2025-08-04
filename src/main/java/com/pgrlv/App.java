54058b850f9e250d3044d340974b02ebc116b5c4
pogorelof <v.pogorelof@gmail.com>
2025-08-04 17:37:48 +0500

    update hibernateutil with keys from .env

--- src/main/java/com/pgrlv/App.java
+++ src/main/java/com/pgrlv/App.java
@@ -1,5 +1,7 @@
 package com.pgrlv;
 
+import com.pgrlv.util.HibernateUtil;
+
 public class App {
 
     public static void main(String[] args) {
--- src/main/java/com/pgrlv/util/HibernateUtil.java
+++ src/main/java/com/pgrlv/util/HibernateUtil.java
@@ -1,8 +1,11 @@
 package com.pgrlv.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.Properties;
 
 
 public class HibernateUtil {
@@ -12,7 +15,9 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
             Configuration configuration = new Configuration().configure();
-
+            Dotenv dotenv = Dotenv.load();
+            configuration.setProperty("hibernate.connection.username", dotenv.get("DB_USERNAME"));
+            configuration.setProperty("hibernate.connection.url", dotenv.get("DB_URL"));
             return configuration.buildSessionFactory();
         } catch (HibernateException e) {
             throw new RuntimeException(e);
