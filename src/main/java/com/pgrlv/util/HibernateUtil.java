adf5045349ee44a34d206f2f27d5ce59a18df32a
pogorelof <v.pogorelof@gmail.com>
2025-08-04 17:31:29 +0500

    make HibernateUtil

--- src/main/java/com/pgrlv/util/HibernateUtil.java
+++ src/main/java/com/pgrlv/util/HibernateUtil.java
@@ -1,4 +1,25 @@
 package com.pgrlv.util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration().configure();
+
+            return configuration.buildSessionFactory();
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
 }
