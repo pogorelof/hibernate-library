264df57a97ae9ef1168e7bde542c388948d6a184
pogorelof <v.pogorelof@gmail.com>
2025-08-07 17:03:43 +0500

    Add app and service logics for add book and author

--- src/main/java/com/pgrlv/App.java
+++ src/main/java/com/pgrlv/App.java
@@ -1,20 +1,73 @@
 package com.pgrlv;
 
-import com.pgrlv.dao.AuthorDao;
 import com.pgrlv.model.Author;
 import com.pgrlv.model.Book;
-import com.pgrlv.model.Reader;
+import com.pgrlv.service.LibraryService;
 import com.pgrlv.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        try (Session session = sessionFactory.openSession()) {
+        Scanner scanner = new Scanner(System.in);
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        LibraryService libraryService = new LibraryService(session);
+
+        String menuText = """
+                Выберите действие:
+                1. Добавить автора
+                2. Добавить книгу
+                3. Регистрация читателя
+                4. Выдача книги
+                5. Возврат книги
+                6. Список книг
+                7. Список книг читателя
+                8. Удаление книги
+                9. Удаление читателя
+                0. Выход
+                """;
+
+        boolean run = true;
+        while (run){
+            System.out.println(menuText);
+            System.out.print("Выбор: ");
+            String choose = scanner.nextLine();
+
+            switch (choose){
+                case "0":
+                    run = false;
+                    break;
+                case "1":
+                    System.out.print("Введите имя автора: ");
+                    String name = scanner.nextLine();
+                    libraryService.addAuthor(name);
+                    break;
+                case "2":
+                    System.out.println("Введите название книги: ");
+                    String title = scanner.nextLine();
+
+                    System.out.println("Введите количество доступных книг: ");
+                    Integer count = Integer.parseInt(scanner.nextLine());
+
+                    System.out.println("Выберите автора:");
+                    List<Author> authors = libraryService.getAllAuthors();
+                    for (int i = 0; i < authors.size(); i++) {
+                        int index = i + 1;
+                        System.out.println(index + ". " + authors.get(i).getName());
+                    }
+                    System.out.print("Выбор: ");
+                    int chooseAuthor = Integer.parseInt(scanner.nextLine()) - 1;
+
+                    Book book = new Book();
+                    book.setTitle(title);
+                    book.setCount(count);
+                    book.setAuthor(authors.get(chooseAuthor));
+
+                    libraryService.addBook(book);
+            }
 
         }
     }
--- src/main/java/com/pgrlv/service/LibraryService.java
+++ src/main/java/com/pgrlv/service/LibraryService.java
@@ -1,4 +1,36 @@
 package com.pgrlv.service;
 
+import com.pgrlv.dao.AuthorDao;
+import com.pgrlv.dao.BookDao;
+import com.pgrlv.dao.ReaderDao;
+import com.pgrlv.model.Author;
+import com.pgrlv.model.Book;
+import org.hibernate.Session;
+
+import java.util.List;
+
 public class LibraryService {
+
+    private AuthorDao authorDao;
+    private BookDao bookDao;
+    private ReaderDao readerDao;
+    public LibraryService(Session session){
+        authorDao = new AuthorDao(session);
+        bookDao = new BookDao(session);
+        readerDao = new ReaderDao(session);
+    }
+
+    public void addAuthor(String name){
+        Author author = new Author();
+        author.setName(name);
+        authorDao.save(author);
+    }
+
+    public List<Author> getAllAuthors(){
+        return authorDao.getAll();
+    }
+
+    public void addBook(Book book){
+        bookDao.save(book);
+    }
 }
