6641c1804e7050a682da93819184e9ed73f84725
pogorelof <v.pogorelof@gmail.com>
2025-08-07 15:45:30 +0500

    make bookDao

--- src/main/java/com/pgrlv/dao/BookDao.java
+++ src/main/java/com/pgrlv/dao/BookDao.java
@@ -1,32 +1,49 @@
 package com.pgrlv.dao;
 
 import com.pgrlv.model.Book;
+import com.pgrlv.util.HibernateUtil;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class BookDao implements Dao<Book>{
+
+    private Session session;
+    public BookDao(Session session){
+        this.session = session;
+    }
+
     @Override
     public Book getById(Integer id) {
-        return null;
+        return session.find(Book.class, id);
     }
 
     @Override
     public List<Book> getAll() {
-        return List.of();
+        return session.createQuery("FROM Book ", Book.class).list();
     }
 
     @Override
     public void save(Book object) {
-
+        session.beginTransaction();
+        session.persist(object);
+        session.getTransaction().commit();
     }
 
     @Override
     public Book update(Book object) {
-        return new Book();
+        session.beginTransaction();
+        Book merged = session.merge(object);
+        session.getTransaction().commit();
+        return merged;
     }
 
     @Override
     public void delete(Book object) {
-
+        if (object != null){
+            session.beginTransaction();
+            session.remove(object);
+            session.getTransaction().commit();
+        }
     }
 }
